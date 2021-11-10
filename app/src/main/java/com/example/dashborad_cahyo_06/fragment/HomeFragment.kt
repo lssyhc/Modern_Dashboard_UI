66a90c94d72406ffb66fe513c7c6e44edad5206a
Cahyo Susilo <cahyosusilo775@gmail.com>
2021-11-10 13:24:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dashborad_cahyo_06/fragment/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.dashborad_cahyo_06.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.dashborad_cahyo_06.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
